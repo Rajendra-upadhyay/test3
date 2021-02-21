3d5e3563153b47ee6bcc42f14ec3d8c1549ef961
Rajendra Upadhyay <rajendra.upadhayay@gmail.com>
2021-02-21 19:34:29 +0530

    test1 added tc5

---
+++ src/test/java/com/restapitest/TC005_getUsers.java
@@ -0,0 +1,36 @@
+package com.restapitest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class TC005_getUsers {
+	
+	@Test
+	public void get_Users_List() {
+		RestAssured.baseURI="https://reqres.in/";
+		
+		RequestSpecification httpRequest=RestAssured.given();
+		
+		Response response=httpRequest.request(Method.GET, "/api/users?page=2");
+		
+		String responseBody=response.getBody().asString();
+		System.out.println("Response body is ::"+responseBody);
+		
+		//status code verification
+		
+		int statusCode=response.getStatusCode();
+		
+		System.out.println("status code is "+statusCode);
+		
+		Assert.assertEquals(statusCode, response.getStatusCode());
+		
+		Assert.assertEquals(statusCode, 201);
+		System.out.println("added by test1 in develop2");
+	}
+
+}
